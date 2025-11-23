5743867466af999c31f107a257835dfb15ad2dc9
Rama <96572530+tetsuyaoy@users.noreply.github.com>
2025-11-23 17:13:22 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-/**
- * main.jsx
- * - BrowserRouter membungkus App sehingga routing bekerja di seluruh aplikasi
- * - React.StrictMode opsional, aman untuk development
- */
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
